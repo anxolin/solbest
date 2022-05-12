63e2450057a6695464ff56be6ee9b9f16bf504ea
Valentin <77051586+vkgnosis@users.noreply.github.com>
2022-05-12 12:39:49 +0200

    oneinch allowance approve

--- src/oneinch.py
+++ src/oneinch.py
@@ -1,20 +1,21 @@
 import requests
 import asyncio
 
+settlement_contract_address = '0x9008D19f58AAbD9eD0D60971565AA8510560ab41',
+
 # TODO:
 # - allowance
 
-async def swap_1inch(sell_token, buy_token, sell_amount):
-    payload = {
+def swap(sell_token, buy_token, sell_amount):
+    params = {
         'fromTokenAddress': sell_token,
         'toTokenAddress': buy_token,
         'amount': sell_amount,
-        # settlement contract
-        'fromAddress': '0x9008D19f58AAbD9eD0D60971565AA8510560ab41',
+        'fromAddress': settlement_contract_address,
         'slippage': 50,
         'disableEstimate': True,
     }
-    r = await asyncio.to_thread(requests.get('https://api.1inch.io/v4.0/100/swap', params=payload))
+    r = requests.get('https://api.1inch.io/v4.0/100/swap', params=params)
     r.raise_for_status()
     r =  r.json()
     return {
@@ -23,7 +24,31 @@ async def swap_1inch(sell_token, buy_token, sell_amount):
       'tx_calldata': r['tx']['data'],
     }
 
+def allowance(token):
+  params = {
+    'tokenAddress': token,
+    'walletAddress': settlement_contract_address,
+  }
+  r = requests.get('https://api.1inch.io/v4.0/100/approve/allowance', params=params)
+  r.raise_for_status()
+  r = r.json()
+  return r['allowance']
+
+def approve(token):
+  params = {
+    'tokenAddress': token,
+    'amount': 2**256 - 1,
+  }
+  r = requests.get('https://api.1inch.io/v4.0/100/approve/transaction', params=params)
+  r.raise_for_status()
+  r =  r.json()
+  return {
+      'tx_to': r['to'],
+      'tx_calldata': r['data'],
+    }
+
 if __name__ == '__main__':
+  if False:
     a = swap_1inch(
         # wxdai
         '0xe91d153e0b41518a2ce8dd3d7944fa863463a97d',
@@ -32,4 +57,10 @@ if __name__ == '__main__':
         # 1e18
         1000000000000000000
     )
+    print(a)
+  if False:
+    a = allowance('0xe91d153e0b41518a2ce8dd3d7944fa863463a97d')
+    print(a)
+  if False:
+    a = approve('0xe91d153e0b41518a2ce8dd3d7944fa863463a97d')
     print(a)
